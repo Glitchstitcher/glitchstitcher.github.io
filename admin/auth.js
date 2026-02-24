75ea3a0a21fe7f19a9531ca18eff5bcd03e8cad3
Glitchstitcher <glitchstitcher@gmail.com>
2026-02-23 17:54:20 -0700

    Update auth.js

--- admin/auth.js
+++ admin/auth.js
@@ -1,7 +1,7 @@
 // --- Configurable Text ---
 const PASSWORD_PAGE_TEXT = {
     title: "🔐 Password",
-    prompt: "Enter the password",
+    // prompt: "Enter the password",
     unlockButton: "Submit",
     logoutButton: "Logout",
     errorMessage: "Incorrect password. Please try again."
